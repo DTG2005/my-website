027998b90bf1ec694ac045b6285ff084d8998001
DaAccursed05 <68813178+DaAccursed05@users.noreply.github.com>
2024-10-02 15:00:40 +0530

    Added image and a primitive navbar

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.githubassets.com",
+        port: "",
+        pathname: "/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        protocol: "https",
+        hostname: "linkedin.com",
+        port: "",
+        pathname: "/static/images/LinkedIn-Mark.png",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
